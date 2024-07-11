5a1ec9b4f5cdd6ecc6b80ece4c0eda8249611921
Gaurav Shamra <sharmagauravxo@gmail.com>
2024-07-11 17:07:33 +0530

    CRUD done

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const seedDB = require("./seed");
 const productRoutes =require('./routes/products');
 const ejsMate=require('ejs-mate');
 const mongoose = require("mongoose");
-
+const methodOverride = require('method-override')
+ 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/shoeping")
@@ -20,6 +23,7 @@ app.engine('ejs', ejsMate)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }))
 
 //seeding DB
 
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,55 @@
-const express =require('express');
+const express = require('express');
 const Product = require('../models/Products');
-const router=express.Router();  //it is a mini instance 
+const router = express.Router();  // it is a mini instance 
 
-
-router.get('/products', async(req, res)=>{
-  let products= await Product.find({})
-    res.render('products/index', {products});
+// To show all the products
+router.get('/products', async (req, res) => {
+  let products = await Product.find({});
+  res.render('products/index', { products });
 });
 
+// To show the form to add the products 
+router.get('/product/new', (req, res) => {
+  res.render('products/new'); 
+});
+//to add the prodcuts in db which came from the form from above form
+router.post('/products',async(req, res)=>{
+   let {name,img, desc, price }=req.body;
+   await Product.create({name, img, desc, price});
+   res.redirect('/products');
+});
+
+//to show a specific product
+router.get('/products/:id',async(req, res)=>{
+  let{id}=req.params;
+  let FoundProduct=await Product.findById(id);
+  res.render('products/show',{FoundProduct})
+})
+
+//to edit a specific product
+
+router.get('/products/:id/edit',async(req, res)=>{
+  let{id}=req.params;
+  let FoundProduct=await Product.findById(id);
+  res.render('products/edit',{FoundProduct});
+})
+
+//to edit the data in db from above form
+
+router.patch('/products/:id', async(req, res)=>{
+  let{id}=req.params;
+  let {name,img, desc, price }=req.body;
+  await Product.findByIdAndUpdate(id,{name,img, desc, price });
+  res.redirect(`/products/${id}`);
+});
+
+// to delete the item
+
+router.delete('/products/:id', async(req, res)=>{
+  let{id}=req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect('/products');
+})
 
 
-
-
-
-module.exports=router;
+module.exports = router;
