b41f534e90627bd706d316d8c4eac06a556a8277
Gaurav Shamra <sharmagauravxo@gmail.com>
2024-07-17 12:55:21 +0530

    login logout added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const User = require('../models/User');
+const passport = require('passport');
 const router = express.Router();
 
+
+//signup
 router.get('/register', (req, res)=>{
     res.render('auth/signup')
 })
@@ -10,11 +13,31 @@ router.post('/register', async (req, res) => {
       let { username, email, password } = req.body;
       const user=new User({username, email})
       const newUser=await User.register(user, password)
-      res.send(newUser);
+      res.redirect('/login');
     } catch (err) {
       res.status(500).render('error', { error: err });
     }
   });
+
+
+  //login
+router.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+
+router.post('/login',passport.authenticate('local', { failureRedirect: '/login' }), (req, res)=>{
+     res.redirect('/products')
+})
+
+
+//logout
+router.get('/logout',(req, res)=>{
+    ()=>{
+        req.logOut();
+    }
+    res.redirect('/login')
+
+})
 
 
 module.exports = router;
